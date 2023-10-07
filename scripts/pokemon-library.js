d2b6ab8e64a09c7adba0472ec4c039997baca7cc
jacobperry <perryjacob@outlook.com>
2023-10-07 07:43:39 +0000

    Final Project

---
+++ scripts/pokemon-library.js
@@ -0,0 +1,68 @@
+
+/// Get all pokemon of input (string) type
+export async function GetPokemonOfType (pokemonType) {
+    const response = await fetch(`https://pokeapi.co/api/v2/type/${pokemonType}/`);
+    const json = await response.json();
+    return json.pokemon;
+}
+
+export function SelectTeam (pokemonList, size = 6) {
+    let indexes = GetRandomArrayOfLength(size, pokemonList.length)
+    console.log(indexes)
+    let pokemonTeam = Array.from(indexes).map(index => pokemonList[index].pokemon)
+    return pokemonTeam;
+}
+
+export async function SelectMoves(pokemon) {
+    const information = await GetPokemonInformation(pokemon)
+    const moves = information.moves
+    const randomList = GetRandomArrayOfLength(4, information.moves.length)
+    const movesList = randomList.map(ID => Capitilize(moves[ID].move.name.replace(/-/g, " ")))
+    return movesList
+}
+
+export function GetRandomArrayOfLength(amount, maxValue) {
+    let randomValues = new Set();
+    let attempts = 0;
+    while(true) {
+        let initSize = randomValues.size;
+        randomValues.add(Math.floor(Math.random() * maxValue))
+        if(randomValues.size == amount || attempts > 20) {
+            break;
+        } else if (randomValues.size == initSize) {
+            attempts++;
+        }
+    }
+    return Array.from(randomValues)
+}
+
+export async function GetPokemonInformation(pokemon) {
+    const response = await fetch(pokemon)
+    const data = await response.json();
+    return data;
+}
+
+async function PopulateSelect() {
+    const response = await fetch("https://pokeapi.co/api/v2/type/");
+    const json = await response.json();
+    const options = await json.results
+        .filter((type) => type.name != "unknown" && type.name != "shadow")
+        .map(type => {
+            const name = type.name;
+            const option = document.createElement('option');
+            option.value = name;
+            option.id = name;
+            option.textContent = Capitilize(name)
+            return option
+        })
+
+    document.querySelector('#type-list').append(...options)
+}
+
+export function Capitilize(input) {
+    return input.split(" ")
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(" ")
+}
+
+PopulateSelect()
---
+++ scripts/project.js
@@ -0,0 +1,58 @@
+import {GetPokemonOfType, GetPokemonInformation, SelectTeam, Capitilize, SelectMoves} from "./pokemon-library.js" 
+
+let withMoves = false;
+
+async function CallApi() {
+    let searchType = document.querySelector("#type-list").value
+    let teamSize = document.querySelector("#size-input").value
+    let pokemon = await GetPokemonOfType(searchType)
+    
+    if(searchType == "") return;
+
+    let team = SelectTeam(pokemon, teamSize)
+    RenderPokemon(team)
+}
+
+function RenderPokemon(team) {
+    const teamElement = document.querySelector("#pokemon-team");
+    teamElement.innerHTML = "";
+    team.forEach(async function (pokemon) {
+        let information = await GetPokemonInformation(pokemon.url);
+        console.log(information)
+       
+        const article = document.createElement('article');
+        
+        const img = document.createElement('img')
+        img.setAttribute('src', information.sprites.front_default)
+        img.setAttribute('alt', pokemon.name.replace("-", " "))
+        
+        const name = document.createElement('h2');
+        name.textContent = Capitilize(pokemon.name.replace(/-/g, " "));
+
+        const typeTags = information.types.map(item => {
+            return `<p>${item.type.name}</p>`
+        })
+
+        article.append(img, name)
+        
+        if(withMoves) {
+            const movesList = document.createElement('ol');
+            let moves = await SelectMoves(pokemon.url)
+            moves = moves.map(move => `<li>${move}</li>`)
+            movesList.innerHTML = moves.join("")
+
+            article.appendChild(movesList)
+        }
+
+        teamElement.appendChild(article)
+    });
+}
+
+document.querySelector("#generate-button").addEventListener("click", (e) => {
+    e.preventDefault();
+    CallApi()
+})
+
+document.querySelector("#moves").addEventListener("change", (e) => {
+    withMoves = e.target.checked
+})
