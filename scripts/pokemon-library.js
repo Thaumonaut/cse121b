b3c28f3e0e56c74b45565cc2201b4fc199773805
jacobperry <perryjacob@outlook.com>
2023-10-07 11:32:21 +0000

    Added Type visualization

--- scripts/pokemon-library.js
+++ scripts/pokemon-library.js
@@ -1,68 +1,88 @@
-
 /// Get all pokemon of input (string) type
-export async function GetPokemonOfType (pokemonType) {
-    const response = await fetch(`https://pokeapi.co/api/v2/type/${pokemonType}/`);
-    const json = await response.json();
-    return json.pokemon;
+export async function GetPokemonOfType(pokemonType) {
+  const response = await fetch(
+    `https://pokeapi.co/api/v2/type/${pokemonType}/`
+  );
+  const json = await response.json();
+  return json.pokemon;
 }
 
-export function SelectTeam (pokemonList, size = 6) {
-    let indexes = GetRandomArrayOfLength(size, pokemonList.length)
-    console.log(indexes)
-    let pokemonTeam = Array.from(indexes).map(index => pokemonList[index].pokemon)
-    return pokemonTeam;
+export function SelectTeam(pokemonList, size = 6) {
+  let indexes = GetRandomArrayOfLength(size, pokemonList.length);
+  let pokemonTeam = Array.from(indexes).map(
+    (index) => pokemonList[index].pokemon
+  );
+  return pokemonTeam;
 }
 
 export async function SelectMoves(pokemon) {
-    const information = await GetPokemonInformation(pokemon)
-    const moves = information.moves
-    const randomList = GetRandomArrayOfLength(4, information.moves.length)
-    const movesList = randomList.map(ID => Capitalize(moves[ID].move.name.replace(/-/g, " ")))
-    return movesList
+  const information = await GetPokemonInformation(pokemon);
+  const moves = information.moves;
+  const randomList = GetRandomArrayOfLength(4, information.moves.length);
+  const movesList = randomList.map(function (ID) {
+    const move = moves[ID].move;
+    return {
+        name: Capitalize(move.name.replace(/-/g, " ")),
+        url: move.url
+    }
+  })
+
+  return movesList;
+}
+
+export async function MovesType(moves) {
+    const movesInfo = await moves.map(async (move) => {
+        const response = await fetch(move.url)
+        const data = await response.json();
+        console.log(data)
+        return data.type.name;
+    });
+    return await Promise.all(movesInfo)
 }
 
 export function GetRandomArrayOfLength(amount, maxValue) {
-    let randomValues = new Set();
-    let attempts = 0;
-    while(true) {
-        let initSize = randomValues.size;
-        randomValues.add(Math.floor(Math.random() * maxValue))
-        if(randomValues.size == amount || attempts > 20) {
-            break;
-        } else if (randomValues.size == initSize) {
-            attempts++;
-        }
+  let randomValues = new Set();
+  let attempts = 0;
+  while (true) {
+    let initSize = randomValues.size;
+    randomValues.add(Math.floor(Math.random() * maxValue));
+    if (randomValues.size == amount || attempts > 20) {
+      break;
+    } else if (randomValues.size == initSize) {
+      attempts++;
     }
-    return Array.from(randomValues)
+  }
+  return Array.from(randomValues);
 }
 
 export async function GetPokemonInformation(pokemon) {
-    const response = await fetch(pokemon)
-    const data = await response.json();
-    return data;
+  const response = await fetch(pokemon);
+  const data = await response.json();
+  return data;
 }
 
 async function PopulateSelect() {
-    const response = await fetch("https://pokeapi.co/api/v2/type/");
-    const json = await response.json();
-    const options = await json.results
-        .filter((type) => type.name != "unknown" && type.name != "shadow")
-        .map(type => {
-            const name = type.name;
-            const option = document.createElement('option');
-            option.value = name;
-            option.id = name;
-            option.textContent = Capitalize(name)
-            return option
-        })
+  const response = await fetch("https://pokeapi.co/api/v2/type/");
+  const json = await response.json();
+  const options = await json.results
+    .filter((type) => type.name != "unknown" && type.name != "shadow")
+    .map((type) => {
+      const name = type.name;
+      const option = document.createElement("option");
+      option.value = name;
+      option.id = name;
+      option.textContent = Capitalize(name);
+      return option;
+    });
 
-    document.querySelector('#type-list').append(...options)
+  document.querySelector("#type-list").append(...options);
 }
 
 export function Capitalize(input) {
-    return input.split(" ")
-        .map(word => `${word.charAt(0).toUpperCase()}${word.slice(1)}`)
-        .join(" ")
+  return input
+    .split(" ")
+    .map((word) => `${word.charAt(0).toUpperCase()}${word.slice(1)}`)
+    .join(" ");
 }
 
-PopulateSelect()
+PopulateSelect();
--- scripts/project.js
+++ scripts/project.js
@@ -1,58 +1,76 @@
-import {GetPokemonOfType, GetPokemonInformation, SelectTeam, Capitalize, SelectMoves} from "./pokemon-library.js" 
+import {
+  GetPokemonOfType,
+  GetPokemonInformation,
+  SelectTeam,
+  Capitalize,
+  SelectMoves,
+  MovesType,
+} from "./pokemon-library.js";
 
 let withMoves = false;
 
 async function CallApi() {
-    let searchType = document.querySelector("#type-list").value
-    let teamSize = document.querySelector("#size-input").value
-    let pokemon = await GetPokemonOfType(searchType)
-    
-    if(searchType == "") return;
+  let searchType = document.querySelector("#type-list").value;
+  let teamSize = document.querySelector("#size-input").value;
+  let pokemon = await GetPokemonOfType(searchType);
 
-    let team = SelectTeam(pokemon, teamSize)
-    RenderPokemon(team)
+  if (searchType == "") return;
+
+  let team = SelectTeam(pokemon, teamSize);
+  RenderPokemon(team);
 }
 
 function RenderPokemon(team) {
-    const teamElement = document.querySelector("#pokemon-team");
-    teamElement.innerHTML = "";
-    team.forEach(async function (pokemon) {
-        let information = await GetPokemonInformation(pokemon.url);
-        console.log(information)
-       
-        const article = document.createElement('article');
-        
-        const img = document.createElement('img')
-        img.setAttribute('src', information.sprites.front_default)
-        img.setAttribute('alt', pokemon.name.replace("-", " "))
-        
-        const name = document.createElement('h2');
-        name.textContent = Capitalize(pokemon.name.replace(/-/g, " "));
+  const teamElement = document.querySelector("#pokemon-team");
+  teamElement.innerHTML = "";
+  team.forEach(async function (pokemon) {
+    let information = await GetPokemonInformation(pokemon.url);
 
-        const typeTags = information.types.map(item => {
-            return `<p>${item.type.name}</p>`
-        })
+    const article = document.createElement("article");
 
-        article.append(img, name)
-        
-        if(withMoves) {
-            const movesList = document.createElement('ol');
-            let moves = await SelectMoves(pokemon.url)
-            moves = moves.map(move => `<li>${move}</li>`)
-            movesList.innerHTML = moves.join("")
+    const img = document.createElement("img");
+    img.setAttribute("src", information.sprites.front_default);
+    img.setAttribute("alt", pokemon.name.replace("-", " "));
 
-            article.appendChild(movesList)
-        }
+    const name = document.createElement("h2");
+    name.textContent = Capitalize(pokemon.name.replace(/-/g, " "));
 
-        teamElement.appendChild(article)
+    const typeTags = information.types.map((item) => {
+      return `<p class="${item.type.name}">${item.type.name}</p>`;
     });
+    const typeDiv = document.createElement("div");
+    typeDiv.className = "type-tags";
+    typeDiv.innerHTML = typeTags.join("");
+
+    article.append(img, name, typeDiv);
+
+    if (withMoves) {
+      const movesList = document.createElement("ul");
+      let moves = await SelectMoves(pokemon.url);
+      let moveTypes = await MovesType(moves);
+      //let typeList = await Promise.all(moveTypes)
+      console.log(moveTypes);
+      moves = await moves.map(function (move, index) {
+        return `<li class="${moveTypes[index]}">${move.name}</li>`;
+      });
+      movesList.innerHTML = moves.join("");
+
+      const h4 = document.createElement("h4");
+      h4.textContent = "Moves";
+
+      article.appendChild(h4);
+      article.appendChild(movesList);
+    }
+
+    teamElement.appendChild(article);
+  });
 }
 
 document.querySelector("#generate-button").addEventListener("click", (e) => {
-    e.preventDefault();
-    CallApi()
-})
+  e.preventDefault();
+  CallApi();
+});
 
 document.querySelector("#moves").addEventListener("change", (e) => {
-    withMoves = e.target.checked
-})
+  withMoves = e.target.checked;
+});
